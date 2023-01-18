660c99e68a08bc03a0f59d9684c5d9eb42b3cfc2
Yao Fu <yao.fu@ed.ac.uk>
2023-01-18 11:46:49 +0000

    update

--- scripts/read_bbh_output.py
+++ scripts/read_bbh_output.py
@@ -1,4 +1,4 @@
-filename = "logs/beta_0.0.2.8_bbh_e1_cot_eval.log"
+filename = "logs/beta_0.0.2.8_bbh_e0_cot_eval.log"
 lines = open(filename).readlines()
 for l in lines:
     if('All average' in l): print(l.split('All average ')[1].split()[0])
